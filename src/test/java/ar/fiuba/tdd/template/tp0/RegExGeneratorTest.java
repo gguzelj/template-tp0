ca5c5168c9d755e0c5da471aa1b993600a6921d1
German Guzelj <german.guzelj@almundo.com>
2016-03-12 10:54:51 -0300

    Testing travis-CI

--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -1,11 +1,15 @@
 package ar.fiuba.tdd.template.tp0;
 
+import com.google.common.collect.Sets;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.google.common.collect.Sets.newHashSet;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class RegExGeneratorTest {
@@ -16,6 +20,8 @@ public class RegExGeneratorTest {
         List<String> results = generator.generate(/*regEx, numberOfResults*/);
         // force matching the beginning and the end of the strings
         Pattern pattern = Pattern.compile("^" + regEx + "$");
+
+
         return results
                 .stream()
                 .reduce(true,
@@ -27,12 +33,17 @@ public class RegExGeneratorTest {
     }
 
     //TODO: Uncomment these tests
-    /*
     @Test
     public void testAnyCharacter() {
-        assertTrue(validate(".", 1));
-    }
+        HashSet<String> one = newHashSet("1", "2", "3");
+        HashSet<String> other = newHashSet("3", "4", "1");
 
+
+        assertEquals(one, other);
+
+//        assertTrue(validate(".", 1));
+    }
+/*
     @Test
     public void testMultipleCharacters() {
         assertTrue(validate("...", 1));
