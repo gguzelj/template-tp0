c69d133085daf0e7a6ec8699aff1d8990db03d67
German Guzelj <german.guzelj@almundo.com>
2016-03-21 13:29:53 -0300

    Literal state bug solved

--- src/main/java/ar/fiuba/tdd/template/tp0/tokenizer/analyzer/states/LiteralState.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/tokenizer/analyzer/states/LiteralState.java
@@ -7,46 +7,50 @@ import ar.fiuba.tdd.template.tp0.tokenizer.helper.QuantifierHelper;
 import ar.fiuba.tdd.template.tp0.tokenizer.quantifier.Quantifier;
 import ar.fiuba.tdd.template.tp0.tokenizer.token.Token;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import static ar.fiuba.tdd.template.tp0.tokenizer.analyzer.states.resolver.StateResolver.DEFAULT_STATE;
-import static ar.fiuba.tdd.template.tp0.tokenizer.analyzer.states.resolver.StateResolver.resolve;
-import static ar.fiuba.tdd.template.tp0.tokenizer.helper.Helper.isEscape;
-import static ar.fiuba.tdd.template.tp0.tokenizer.helper.Helper.isEscaped;
-import static ar.fiuba.tdd.template.tp0.tokenizer.helper.Helper.isLiteral;
+import static ar.fiuba.tdd.template.tp0.tokenizer.helper.Helper.*;
 import static ar.fiuba.tdd.template.tp0.tokenizer.helper.QuantifierHelper.hasQuantifier;
 import static ar.fiuba.tdd.template.tp0.tokenizer.helper.QuantifierHelper.isQuantifier;
 import static ar.fiuba.tdd.template.tp0.tokenizer.token.TokenType.LITERAL;
 import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 
 public class LiteralState implements State {
 
     private Character literal;
     private Optional<Quantifier> quantifier;
+    private Boolean escapingEscape; //don't like this...
 
     @Override
     public Optional<Token> resolveToken(Context context, Analyzer analyzer) {
 
         checkContext(context);
 
-        if (isNull(this.literal)) {
-            this.literal = getLiteral(context);
-            return (isEscape(context) || hasQuantifier(context)) ? Optional.empty() : returnToken(analyzer);
-        }
-
-        if (hasQuantifier(context) && isNull(this.quantifier)) {
-            this.quantifier = QuantifierHelper.resolveQuantifier(context);
+        if (isEscape(context) && isNull(this.escapingEscape)) {
+            this.escapingEscape = escapingEscape(context);
             return Optional.empty();
         }
 
-        return returnToken(analyzer);
+        if (isNull(this.literal)) {
+            this.literal = context.getCharacter();
+        }
+
+        if (hasQuantifier(context) && isNull(this.quantifier)) {
+            this.quantifier = Optional.empty();
+            return Optional.empty();
+        }
+
+        return returnToken(analyzer, context);
     }
 
-    private Optional<Token> returnToken(Analyzer analyzer) {
+    private Boolean escapingEscape(Context context) {
+        return isEscape(context.getNextCharacter().get()) ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+    private Optional<Token> returnToken(Analyzer analyzer, Context context) {
         analyzer.setState(DEFAULT_STATE);
-        return newToken();
+        return newToken(context);
     }
 
     private void checkContext(Context context) {
@@ -55,19 +59,13 @@ public class LiteralState implements State {
         }
     }
 
-    private Optional<Token> newToken() {
+    private Optional<Token> newToken(Context context) {
         final Character character = literal;
-        final Optional<Quantifier> quantifier = isNull(this.quantifier) ? Optional.empty() : this.quantifier;
+        final Optional<Quantifier> quantifier = isNull(this.quantifier) ? Optional.empty() : QuantifierHelper.resolveQuantifier(context);
         this.literal = null;
         this.quantifier = null;
+        this.escapingEscape = null;
         return Optional.of(new Token(LITERAL, quantifier, character));
-    }
-
-    private Character getLiteral(Context context) {
-        if (isEscape(context)) {
-            return context.getNextCharacter().get();
-        }
-        return context.getCharacter();
     }
 
     @Override
